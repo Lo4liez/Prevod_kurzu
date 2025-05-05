ea6304c4c11e691610e020717ccff7a877f537f3
Suponin Maksym <L20220125@prumyslovkajihlava.cz>
2025-05-05 13:06:39 +0200

    528

--- vite-project/src/components/Prevod.jsx
+++ vite-project/src/components/Prevod.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
 export const Prevod = () => {
     const [cislo, setCislo] = useState(0);
@@ -6,23 +6,21 @@ export const Prevod = () => {
 
     const getKurzy = async () => {
         try{
-            const response = await fetch("https://api.frankfurter.dev/v1/latest?base=EUR")
+            const response = await fetch("https://api.frankfurter.dev/v1/latest?base=EUR");
             const data = await response.json();
+            setKurzy(data);
         }catch(error){
             console.log(error);
         }
     };
-
     useEffect(() =>{
-
-    },[]);
-
+        getKurzy();
+    },[kurzy]);
 return(
     <div>
         <h1>
             Oi oi Hughie prevedeme na Euro
         </h1>  
-
         <div>
             <input
                 type ="number"
@@ -30,7 +28,11 @@ return(
                 onChange={(e) => setCislo(e.target.value)}
                 style={{padding: '5px'}}
             />
-            <select></select>
+            <select>
+                {
+                Object.entries(kurzy).map(([key,value]) =>{<option value={value}>{key}</option>})
+                }
+                </select>
             <button>Preved</button>
         </div>
     </div>        
